5ff67fa31c401360b8a6c6c8dd878ac3850775da
dejan <dejan@Dejans-MacBook-Pro.local>
2020-06-21 11:58:54 +0200

    Rendering Project Lists.

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,34 @@ function autobind(
 	return adjustedDescriptor;
 }
 
+// project list class
+class ProjectList {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLElement;
+
+	constructor(private type: 'active' | 'finished') {
+		this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+		this.hostElement = <HTMLDivElement> document.getElementById('app')!;
+		const importedNode = document.importNode(this.templateElement.content, true);
+		this.element = importedNode.firstElementChild as HTMLElement;
+
+		this.element.id = `${this.type}-projects`;
+		this.attach();
+		this.renderContent();
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement('beforeend', this.element);
+	}
+
+	private renderContent() {
+		const listId = `${this.type}-projects-list`;
+		this.element.querySelector('ul')!.id = listId;
+		this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+	}
+}
+
 // project input class
 class ProjectInput  {
 	templateElement: HTMLTemplateElement;
@@ -129,3 +157,5 @@ class ProjectInput  {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
