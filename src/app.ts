bd1dd99ee2b6698cc82554922671fec0bc29cf77
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-12 10:32:17 +0200

    Getting user input.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,8 @@
-import { Product } from './product.model';
-import 'reflect-metadata';
-import { plainToClass } from 'class-transformer';
-
-// const p1 = new Product('A book', 12.99)
-const products = [ {title: 'A carpet', price: 29.99}, { title:'A book', price: 10.99}]
-// const loadedProducts = products.map(prod => {
-// 	return new Product(prod.title, prod.price);
-// });
-const loadedProducts = plainToClass(Product, products);
-
-for (const prod of loadedProducts) {
-	console.log(prod.getInformation());
+const form  = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+function searchAddressHandler(event: Event) {
+event.preventDefault();
+const enteredAddress = addressInput.value;
 }
+
+form.addEventListener('submit', searchAddressHandler)
