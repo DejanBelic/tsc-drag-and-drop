b0b158a5d38c71cd12d1ab9dccbf3ba77e9b85a6
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-12 10:12:59 +0200

    TypeScript-emracing class-validator.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import {ProjectInput} from "./components/project-input";
-import {ProjectList} from "./components/project-list";
+import { Product } from './product.model';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 
-	new ProjectInput();
-	new ProjectList('active');
-	new ProjectList('finished');
+// const p1 = new Product('A book', 12.99)
+const products = [ {title: 'A carpet', price: 29.99}, { title:'A book', price: 10.99}]
+// const loadedProducts = products.map(prod => {
+// 	return new Product(prod.title, prod.price);
+// });
+const loadedProducts = plainToClass(Product, products);
 
+for (const prod of loadedProducts) {
+	console.log(prod.getInformation());
+}
