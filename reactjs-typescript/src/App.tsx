4c42a03698ab2a829687ec4290096aa796b0c33d
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-13 18:24:10 +0200

    Working with Props and Types for Props.

--- reactjs-typescript/src/App.tsx
+++ reactjs-typescript/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoList from "./components/ToDoList";
 
 function App() {
+	const todos = [ { id: 't1', text: 'Finish the course'}];
   return (
     <div className="App">
-
+		<TodoList items={todos}/>
     </div>
   );
 }
