ba52a65507153626021802d6ff86a839eb5fc554
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-15 20:54:03 +0200

    More props & state work.

--- reactjs-typescript/src/App.tsx
+++ reactjs-typescript/src/App.tsx
@@ -6,12 +6,17 @@ import {Todo} from './todo.model';
 function App() {
 	const [todos, setTodos] = useState<Todo[]>([])
 	const todoAddHandler = (text: string) => {
-		setTodos([ { id: 't1', text: 'Finish the course'}])
+		setTodos(prevTodos => [...prevTodos, { id: 't1', text: 'Finish the course'}])
+	}
+	const todoDeleteHandler = (todoId: string) => {
+		setTodos(prevTodos => {
+			return prevTodos.filter(todo => todo.id !== todoId);
+		})
 	}
   return (
     <div className="App">
 		<NewTodo onAddTodo={todoAddHandler}/>
-		<TodoList items={todos}/>
+		<TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
     </div>
   );
 }
