c400f35400a478e844949ce1a611111ab8867727
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-16 19:22:21 +0200

    More on Type Guards.

--- src/app.ts
+++ src/app.ts
@@ -21,3 +21,52 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+	if (typeof a === 'string' || typeof b === 'string') {
+		return a.toString() + b.toString();
+	}
+	return a + b;
+}
+
+type UnknownEmployee = Admin | Employee;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+	console.log(emp.name);
+	if ('privileges' in emp) {
+		console.log(emp.privileges)
+	}
+	if ('startDate' in emp) {
+		console.log(emp.startDate)
+	}
+}
+
+class Car {
+	drive() {
+		console.log('Drive car');
+	}
+}
+
+class Truck {
+	drive() {
+		console.log('Drive truck')
+	}
+	loadCargo(amount: number) {
+		console.log('Loading cargo', amount)
+	}
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+	vehicle.drive();
+	if (vehicle instanceof Truck) {
+		vehicle.loadCargo(1000);
+	}
+}
+
+useVehicle(v1);
+useVehicle(v2);
