f99d99da3e0da139058599992f3050c162cbae78
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-16 18:54:42 +0200

    Intersection Types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+type Admin = {
+	name: string;
+	privileges: string[];
+};
+
+type Employee = {
+	name: string;
+	startDate: Date;
+}
+
+type ElevatedEmployee = Admin & Employee;
+// interface ElevatedEmployee  extends  Admin, Employee {};
+
+const e1: ElevatedEmployee = {
+	name: 'Dejan',
+	privileges: ['create-server'],
+	startDate: new Date()
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
