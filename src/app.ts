3c33441e292e506aa848c9a878f4589923b80570
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-18 19:54:19 +0200

    Another generic function.

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,19 @@ return Object.assign(objA, objB);
 const mergedObj = 	merge({name: 'Dejan'}, {age: 28})
 
 console.log(mergedObj.name)
+
+interface Lengthy {
+	length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+	let descriptionText = 'Got no value';
+	if (element.length === 1) {
+		descriptionText = 'Got 1 element';
+	} else if (element.length > 1) {
+		descriptionText = 'Got ' + element.length  + 'elements';
+	}
+	return [element, descriptionText]
+}
+
+console.log(countAndDescribe('Hi there'))
