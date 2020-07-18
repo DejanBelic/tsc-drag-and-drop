2a876a118132a68460731aeff72686f424109922
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-18 20:15:14 +0200

    Generic classes.

--- src/app.ts
+++ src/app.ts
@@ -27,3 +27,30 @@ function extractAndConvert<T extends Object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({name: 'Dejan'}, 'name'));
+
+// <T extends string |  number | boolean>  meaning DataStorage will work only with these types numbers strings booleans
+class DataStorage<T extends string |  number | boolean> {
+	private data: T[] = [];
+
+	addItem(item: T) {
+		this.data.push(item);
+	}
+
+	removeItem(item: T) {
+		if (this.data.indexOf(item) === -1) {
+			return;
+		}
+		this.data.splice(this.data.indexOf(item),1);
+	}
+
+	getItems() {
+		return [...this.data];
+	}
+}
+
+const textStorage = new DataStorage<string>();
+
+textStorage.addItem('Dejan');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
