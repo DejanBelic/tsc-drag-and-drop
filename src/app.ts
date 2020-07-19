94611b5210418f0f9e348679587cffe52361c54c
dejan <dejan@Dejans-MacBook-Pro.local>
2020-07-19 12:47:09 +0200

    Diving into Property Decorators.

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ function Logger(logString: string) {
 	}
 }
 
+function Log(target: any, propertyName: string | Symbol) {
+	console.log('Property decorator!');
+	console.log(target, propertyName);
+}
+
 function withTemplate(template: string, hookId: string) {
 	return function (constructor: any) {
 		const hookEl = document.getElementById(hookId);
@@ -16,7 +21,7 @@ function withTemplate(template: string, hookId: string) {
 	}
 }
 
-// @Logger('LOGGIN - PERSON')
+@Logger('LOGGIN - PERSON')
 @withTemplate('<h1>My person object </h1>', 'app')
 class Person {
 	name = 'Dejan';
@@ -28,3 +33,26 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+class Product {
+	@Log
+	title: string;
+	private _price: number;
+
+	set price(val: number) {
+		if (val > 0) {
+			this._price = val
+		} else {
+			throw new Error('Invalid price should be positive.')
+		}
+	}
+
+	constructor(t: string, p: number) {
+		this.title = t;
+		this._price = p;
+	}
+
+	getPriceWithTax(tax: number) {
+		return this._price * (1 + tax);
+	}
+}
